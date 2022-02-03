82e3a478c962c402f61e66dfe8d61bd26f496486
Ian Seddon <ianseddon55@gmail.com>
2022-02-03 16:49:01 -0700

    Update exports for library usage.

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,22 @@
-import Engine from "./core/Engine";
+import { Component } from "./core/Component";
 import { Entity } from "./core/Entity";
-import { CanvasContext, Circle, Movement } from "./demo/components";
-import { MovementSystem, Renderer } from "./demo/systems";
+import { EntityManager } from "./core/EntityManager";
+import { EntityQuery } from "./core/EntityQuery";
+import { Engine } from "./core/Engine";
+import { Node } from "./core/Node";
+import { System } from "./core/System";
+import { SystemManager } from "./core/SystemManager";
+import { UniqueId } from "./core/UniqueId";
 
-const random = (min: number, max: number): number => Math.floor((Math.random() * (max - min)) + min);
-
-const engine = new Engine();
-
-const canvas = document.querySelector('canvas');
-if (!canvas) {
-  throw new Error('No canvas element!');
-}
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const ctx = canvas.getContext('2d');
-if (!ctx) {
-  throw new Error('Could not get canvas context');
-}
-const canvasEntity = new Entity();
-const canvasContext = new CanvasContext(ctx);
-canvasContext.width = canvas.width;
-canvasContext.height = canvas.width;
-canvasEntity.add(canvasContext);
-engine.entityManager.addEntity(canvasEntity);
-
-window.addEventListener('resize', () => {
-  canvasContext.width = canvas.width;
-  canvasContext.height = canvas.height;
-});
-
-for (let i = 0; i < 30; i++) {
-  const entity = engine.entityManager.createEntity();
-  const circle = new Circle();
-  const movement = new Movement();
-
-  circle.position.x = random(0, canvas.width);
-  circle.position.y = random(0, canvas.height);
-  circle.radius = random(20, 100);
-  movement.velocity.x = random(-20, 20);
-  movement.velocity.y = random(-20, 20);
-
-  entity.add(circle);
-  entity.add(movement);
-  engine.entityManager.addEntity(entity);
-}
-
-engine.systemManager.addSystem(new Renderer);
-engine.systemManager.addSystem(new MovementSystem);
-
-// Set up the update loop.
-let lastTime = performance.now();
-const update = () => {
-  const time = performance.now();
-  const delta = (time - lastTime) / 1000;
-  lastTime = time;
-  engine.update(delta);
-  requestAnimationFrame(update);
-}
-
-update();
+const Ecsellent = {
+    Component,
+    Engine,
+    Entity,
+    EntityManager,
+    EntityQuery,
+    Node,
+    System,
+    SystemManager,
+    UniqueId,
+};
+export default Ecsellent;
