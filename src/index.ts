6f30cea223971a2a9316627919541afb46f93412
Ian Seddon <ianseddon55@gmail.com>
2022-02-03 17:14:00 -0700

    Update index.ts exports.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
-import { Component } from "./core/Component";
-import { Entity } from "./core/Entity";
-import { EntityManager } from "./core/EntityManager";
-import { EntityQuery } from "./core/EntityQuery";
-import { Engine } from "./core/Engine";
-import { Node } from "./core/Node";
-import { System } from "./core/System";
-import { SystemManager } from "./core/SystemManager";
-import { UniqueId } from "./core/UniqueId";
-
-const Ecsellent = {
-    Component,
-    Engine,
-    Entity,
-    EntityManager,
-    EntityQuery,
-    Node,
-    System,
-    SystemManager,
-    UniqueId,
-};
-export default Ecsellent;
+export { Component } from "./core/Component";
+export { Entity } from "./core/Entity";
+export { EntityManager } from "./core/EntityManager";
+export { EntityQuery } from "./core/EntityQuery";
+export { Engine } from "./core/Engine";
+export { Node } from "./core/Node";
+export { System } from "./core/System";
+export { SystemManager } from "./core/SystemManager";
+export { UniqueId } from "./core/UniqueId";
